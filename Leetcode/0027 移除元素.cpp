1065621744ba30bdfa431a2e08b1caccce8a502c
iJayKing <120683967+iJayKing@users.noreply.github.com>
2026-04-26 21:36:46 +0800

    0027 移除元素.cpp

---
+++ Leetcode/0027 移除元素.cpp
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <vector>
+
+class Solution {
+public:
+	int removeElement(vector<int>& num, int val)
+	{
+		int slowIndex = 0;
+		for ( int fastIndex = 0;fastIndex < num.size();fastIndex++ )
+		{
+			if (num[fastIndex] != val)
+			{
+				num[slowIndex] = num[fastIndex];
+				slowIndex++;
+			}
+		}
+		return slowIndex;
+	}
+};
